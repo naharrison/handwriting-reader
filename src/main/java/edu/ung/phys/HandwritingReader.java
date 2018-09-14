2ca84df3009f8101fd15a8f3667e9119d7b156b0
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2018-09-13 22:04:53 -0400

    It works now but needs fine tuning

--- src/main/java/edu/ung/phys/HandwritingReader.java
+++ src/main/java/edu/ung/phys/HandwritingReader.java
@@ -4,6 +4,19 @@ import processing.core.PApplet;
 
 import java.util.Collections;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
 /**
  * @author naharrison
@@ -12,28 +25,86 @@ import java.util.ArrayList;
  */
 public class HandwritingReader extends PApplet {
 
-	public static void main(String[] args) {
-		PApplet.main("edu.ung.phys.HandwritingReader");
-	}
+  public static void main(String[] args) {
+    PApplet.main("edu.ung.phys.HandwritingReader");
+  }
 
 
   public int frRate;
   public ArrayList<Integer> pixels;
-
-	
-	public void settings() {
-		size(28*20, 28*20);
-	}
+  public BasicNetwork network;
 
 
-	public void setup() {
-		frRate = 60;
-		frameRate(frameRate);
+  public void settings() {
+    size(28*20, 28*20);
+  }
+
+
+  public void setup() {
+    frRate = 60;
+    frameRate(frameRate);
     pixels = new ArrayList<Integer>(Collections.nCopies(28*28, 0));
-	}
 
-	
-	public void draw() {
+    network = new BasicNetwork();
+    network.addLayer(new BasicLayer(null, true, 28*28)); // input layer
+    network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 20)); // hidden layer with 20 neurons
+    network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 20)); // hidden layer with 20 neurons
+    network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 10)); // output layer, answer can be 0-9
+    network.getStructure().finalizeStructure();
+    network.reset();
+
+    try {
+      trainNetwork();
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  public void trainNetwork() throws IOException {
+    MLDataSet trainingSet = new BasicMLDataSet();
+    BufferedReader dataReader = createReader("mnist_10k.txt");
+    String currentLine = dataReader.readLine();
+    while(currentLine != null) {
+      String[] currentLineParts = currentLine.split(",");
+      BasicMLData inputData = getInputData(currentLineParts);
+      BasicMLData idealOutputData = getIdealOutputData(Integer.parseInt(currentLineParts[0]));
+      trainingSet.add(inputData, idealOutputData);
+      currentLine = dataReader.readLine();
+    }
+    dataReader.close();
+
+    final ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+    int epoch = 1;
+    while(train.getError() > 0.03 || epoch == 1) {
+      train.iteration();
+      System.out.println(epoch + " " + train.getError());
+      epoch++;
+    }
+    train.finishTraining();
+    System.out.println("");
+  }
+
+
+  public BasicMLData getIdealOutputData(int correctDigit) {
+    double[] idealOutput = new double[10];
+    for(int k = 0; k < 10; k++) idealOutput[k] = 0.0;
+    idealOutput[correctDigit] = 1.0;
+    return new BasicMLData(idealOutput);
+  }
+
+
+  public BasicMLData getInputData(String[] csvValues) {
+    double[] inputInts = new double[csvValues.length - 1];
+    for(int k = 1; k < csvValues.length; k++) {
+      inputInts[k-1] = Double.parseDouble(csvValues[k]);
+    }
+    return new BasicMLData(inputInts);
+  }
+
+
+  public void draw() {
     background(200);
 
     int ix = 0;
@@ -55,14 +126,51 @@ public class HandwritingReader extends PApplet {
       line(0, k*height/28, width, k*height/28);
       line(k*width/28, 0, k*width/28, height);
     }
-	}
+
+    fill(75, 225, 75);
+    ellipse(50, height - 50, 40, 40);
+    fill(0, 0, 255);
+    ellipse(100, height - 50, 40, 40);
+  }
 
 
-	public void mouseDragged() {
+  public void mouseDragged() {
     int ix = mouseX/(width/28);
     int iy = mouseY/(height/28);
     int i = iy*28 + ix;
     pixels.set(i, 255);
-	}
+  }
+
+
+  public void mouseClicked() {
+    if(Math.sqrt(Math.pow(mouseX - 50, 2.0) + Math.pow(mouseY - (height-50), 2.0)) < 40) {
+      System.out.println("green button clicked");
+      queryNetwork();
+    }
+
+    if(Math.sqrt(Math.pow(mouseX - 100, 2.0) + Math.pow(mouseY - (height-50), 2.0)) < 40) {
+      System.out.println("blue button clicked - pixels reset");
+      System.out.println("");
+      for(int k = 0; k < 28*28; k++) pixels.set(k, 0);
+    }
+  }
+
+
+  public void queryNetwork() {
+    double[] pixelArray = new double[28*28];
+    for(int k = 0; k < 28*28; k++) {
+      pixelArray[k] = (double) pixels.get(k);
+    }
+    BasicMLData queryData = new BasicMLData(pixelArray);
+    final MLData networkOutputData = network.compute(queryData);
+    ArrayList<Double> networkOutput = new ArrayList<>();
+    for(int k = 0; k < 10; k++) networkOutput.add(networkOutputData.getData(k));
+    for(int k = 0; k < 10; k++) {
+      System.out.print(k + " " + networkOutput.get(k) + " ");
+      if(networkOutput.get(k) == Collections.max(networkOutput)) System.out.print("<-----");
+      System.out.println("");
+    }
+    System.out.println("");
+  }
 
 }
